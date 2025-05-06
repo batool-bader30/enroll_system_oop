bb0ca6e81ef038bc0c008284f1cb26655501124d
batool-bader30 <batool.osama304@gmail.com>
2025-05-06 03:02:41 +0300

    update

---
+++ models/student.dart
@@ -0,0 +1,10 @@
+class Students {
+  String name;
+  int id;
+
+  Students({required this.name, required this.id});
+  void displayInfo() {
+    print("name : $name");
+    print("id : $id");
+  }
+}
